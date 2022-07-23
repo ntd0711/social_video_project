9688053ebe410f52fb9178819b04fef663680739
ntd0711 <ntd07112000@gmail.com>
2022-07-23 21:41:55 +0700

    add domain url image

--- next.config.js
+++ next.config.js
@@ -3,11 +3,13 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: [
-      "lh3.googleusercontent.com",
-      "cdn.pixabay.com",
-      "p16-amd-va.tiktokcdn.com",
-      "image.shutterstock.com",
-      "res.cloudinary.com",
+      'lh3.googleusercontent.com',
+      'cdn.pixabay.com',
+      'p16-amd-va.tiktokcdn.com',
+      'image.shutterstock.com',
+      'res.cloudinary.com',
+      'scontent-hkg4-1.cdninstagram.com',
+      'scontent-hkg4-2.cdninstagram.com',
     ],
   },
 };
